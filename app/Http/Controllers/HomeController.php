f6840422bdca876992e6fb160b123fe734281f5a
EliasTG <tg.elias.s@gmail.com>
2021-09-13 13:06:11 -0500

    Adding chart and indicators

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +23,27 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        
+        $pen = count(DB::select("SELECT status FROM `orders` WHERE status like 'PENDIENTE'"));
+        $rec = count(DB::select("SELECT status FROM `orders` WHERE status like 'RECIBIDO'"));
+        $dis = count(DB::select("SELECT status FROM `orders` WHERE status like 'DISTRIBUIDO'"));
+
+        $output = DB::select("SELECT SUM(quantity) as quantity FROM `outputs` INNER JOIN output_details ON outputs.id = output_details.output_id;");
+        $outputAlm = $output[0]->quantity;
+
+        $costAlm = 200;
+        $Alm = DB::select("SELECT SUM(quantity) as quantity FROM `orders` INNER JOIN order_details ON orders.id = order_details.order_id;");
+        
+        $uniAlm = $Alm[0]->quantity;
+
+        $cosUniAlm = round($costAlm/$uniAlm,2);
+        $cosUniServ = round($costAlm/$outputAlm,2);
+        $cosXm = round($costAlm/150,2);
+
+
+
+
+
+        return view('home', compact('cosUniAlm','cosUniServ','cosXm','pen','rec','dis'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,60 @@
 @extends('layout.template')
 @section('content')
-    home
+
+    <div class="row">
+        <div class="col-md-6">
+            <div class="tile">
+                <h3 class="tile-title"> <i class="fas fa-chart-bar"></i> Chart</h3>
+                <div class="embed-responsive embed-responsive-16by9">
+                    <canvas class="embed-responsive-item" id="pedidos"></canvas>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="indicators"><br>
+                <h3 class="tile-title"> <i class="fas fa-info-circle"></i> Indicators</h3><br>
+                <h4>Coste Unitario de Almacenamiento</h4><h4 class="movD">{{$cosUniAlm}}</h4><br>
+                <h4>Coste por unidad servida</h4><h4 class="movD">{{$cosUniServ}}</h4><br>
+                <h4>Coste por m<sup>3</sup></h4><h4 class="movD">{{$cosXm}}</h4>
+                
+                
+            </div>
+        </div>
+    </div>
+
+@endsection
+@section('script')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"></script>
+    <script>
+        var ctx = document.getElementById('pedidos').getContext('2d');
+        var myChart = new Chart(ctx, {
+            type: 'pie',
+            data: {
+                labels: ['Pendiente', 'Recibido', 'Distribuido'],
+                datasets: [{
+                    data: [{{$pen}}, {{$rec}} ,{{$dis}}],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(25, 132, 12, 1)',
+                        'rgba(42, 114, 122, 1)',
+                        'rgba(230, 130, 215, 1)',
+                        'rgba(128, 190, 120, 1)'
+                    ]
+                }]
+            },
+            options: {
+                responsive: true,
+                plugins: {
+                    legend: {
+                        position: 'top',
+                    },
+                    title: {
+                        display: true,
+                    }
+                }
+            },
+        });
+    </script>
 @endsection
